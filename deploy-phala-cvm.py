1f6fdcffcfa105228618ed3fd93c3e1e214eab76
Danny Irving <feng73440@gmail.com>
2025-08-21 15:16:57 +0800

    更新 deploy-phala-cvm.py

--- deploy-phala-cvm.py
+++ deploy-phala-cvm.py
@@ -104,7 +104,7 @@ services:
       - "8332:8332"
     volumes:
       - /var/run/tappd.sock:/var/run/tappd.sock
-      - tee:/home/bitcoin/.bitcoin
+      - datadir:/home/bitcoin/.bitcoin
     environment:
       - TEE_MODE=PRODUCTION
     restart: always
@@ -126,7 +126,7 @@ services:
     restart: unless-stopped
 
 volumes:
-    tee:"""
+    datadir:"""
 
     vm_config = {
         "name": "bitcoin_monitor",
