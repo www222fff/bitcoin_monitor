af0034a0588f9994bf2fdb4a59b9c84870886e15
www222fff <feng73440@gmail.com>
2025-08-22 10:42:11 +0800

    update

--- deploy-phala-cvm.py
+++ deploy-phala-cvm.py
@@ -104,7 +104,6 @@ services:
       - "8332:8332"
     volumes:
       - /var/run/tappd.sock:/var/run/tappd.sock
-      - datadir:/home/bitcoin/.bitcoin
     environment:
       - TEE_MODE=PRODUCTION
     restart: always
@@ -126,7 +125,7 @@ services:
     restart: unless-stopped
 
 volumes:
-    datadir:"""
+    tee:"""
 
     vm_config = {
         "name": "bitcoin_monitor",
